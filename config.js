7907eda74e0005b038edb7c485b5251d2334fd5d
maeda0404 <maeda.kento.au@obayashi.co.jp>
2026-08-26 16:57:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,13 @@
 'use strict';
-window.APP_CONFIG=Object.freeze({dataUrl:'./data/current.json',pointCode:'46106',pointName:'横浜',areaName:'神奈川県',refreshMs:300000,timeoutMs:15000,stale15:900000,stale30:1800000,storageKey:'tsurugamine-wbgt-v2'});
+
+window.APP_CONFIG = Object.freeze({
+  dataUrl: './data/current.json',
+  pointCode: '46106',
+  pointName: '横浜',
+  areaName: '神奈川県',
+  refreshMs: 300000,
+  timeoutMs: 15000,
+  stale15: 900000,
+  stale30: 1800000,
+  storageKey: 'tsurugamine-wbgt-v3'
+});
